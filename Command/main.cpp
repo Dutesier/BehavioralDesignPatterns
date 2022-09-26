0ba6c8365089e01f3bcb08c22a51e45c60387909
Duarte Areias dos Reis <duarte.reis@criticaltechworks.com>
2022-09-26 09:29:51 +0100

    Commands allowing for reusability

--- Command/main.cpp
+++ Command/main.cpp
@@ -5,24 +5,99 @@ public:
     void fullStop(){
         direction = 90;
     }
-    void turn(unsigned int degree) {
+    void turn(int degree) {
         degree = degree % 360;
         direction += degree;
     }
+
+    void printDirection() const {
+        std::cout << "Current direction: " << direction << std::endl;
+    }
 private:
-    unsigned int direction;
+    unsigned int direction = 90;
 };
 
-class IDirectionCommand{
+// Commands
+class ICommand{
 public:
-    virtual ~IDirectionCommand(){};
-    virtual void change_direction() = 0;
+    virtual ~ICommand(){};
+    virtual void execute() = 0;
 };
 
-class leftTurn: public IDirectionCommand{
+class leftTurnCommand: public ICommand{
 public:
-    
+    leftTurnCommand(Car *car) : car(car) {}
+    void execute(){
+        car->turn(15);
+    }
 private:
     Car* car;
-
 };
+
+class rightTurnCommand: public ICommand{
+public:
+    rightTurnCommand(Car *car) : car(car) {}
+    void execute(){
+        car->turn(-15);
+    }
+private:
+    Car* car;
+};
+
+class stopCommand: public ICommand {
+public:
+    stopCommand(Car *car): car(car) {
+
+    }
+    void execute() override{
+        car->fullStop();
+    }
+private:
+    Car* car;
+};
+
+// Inputs
+class Input {
+public:
+    Input(ICommand* command): command(command){}
+    void trigger() {
+        command->execute();
+    }
+private:
+    ICommand *command;
+};
+
+
+
+int main() {
+    Car *car = new Car;
+
+    ICommand* leftTurn = new leftTurnCommand(car);
+    ICommand* rightTurn = new rightTurnCommand(car);
+    ICommand* stop = new stopCommand(car);
+    Input* turnLeftInput = new Input(leftTurn);
+    Input* turnRightInput = new Input(rightTurn);
+    Input* emergencyInput = new Input(stop);
+
+    car->printDirection();
+    turnLeftInput->trigger();
+
+    car->printDirection();
+    for (int turns = 0; turns < 5; ++turns) {
+        turnRightInput->trigger();
+    }
+
+    car->printDirection();
+    emergencyInput->trigger();
+    car->printDirection();
+
+    delete leftTurn;
+    delete rightTurn,
+    delete stop;
+
+    delete turnLeftInput;
+    delete turnRightInput;
+    delete emergencyInput;
+
+    delete car;
+}
